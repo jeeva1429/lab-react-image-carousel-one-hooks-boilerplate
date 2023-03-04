d947918acdabd5126d7d1ddcc18e9b554c4ba9b4
jeeva parkkavan <jeeva.parkkavan@kalvium.community>
2023-03-04 15:51:01 +0530

    changes made

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+const ImageCarousal = (props) => {
+  const [imageNum, setImageNum] = useState(0)
 
-// complete the function below:
-function Carousel() {
+  const nextImage = () => {
+    if (imageNum < images.length - 1)
+      setImageNum(imageNum => imageNum+1);
+  }
+  
+
+  const previousImage = () => {
+    if (imageNum > 0)
+        setImageNum(imageNum => imageNum-1)
+  }
+  
+  return (
+    <div >
+      <div className="title"><h1>{images[imageNum].title}</h1></div>
+      <div id='container'>
+        <div onClick={previousImage} > <ArrowBackIosNewIcon /></div>
+        <img src={images[imageNum].img} alt={images[imageNum].alt} />
+        <div onClick={nextImage}> <ArrowForwardIosIcon /></div>
+      </div>
+      <div className="subtitle"><p>{images[imageNum].subtitle}</p></div>
+    </div>
+  );
 }
 
-export default Carousel;
+export default ImageCarousal;
